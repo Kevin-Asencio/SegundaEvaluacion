0068899313c908d66dcf40b70a7477c50e624082
Kevin-Asencio <54476255+Kevin-Asencio@users.noreply.github.com>
2019-11-02 19:00:37 -0600

    Initial commit

---
+++ app/src/main/java/com/example/segundaevaluacion/ActivityDatos.java
@@ -0,0 +1,14 @@
+package com.example.segundaevaluacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityDatos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_datos);
+    }
+}
---
+++ app/src/main/java/com/example/segundaevaluacion/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.segundaevaluacion;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setTitle("HS16-I04-001");
+        }
+    }
+    public void OnClickCalculadora(View v){
+        Intent i = new Intent(this,ActivityCalculadora.class);
+        startActivity(i);
+    }
+    public void OnClickDatos(View v){
+        Intent i = new Intent(this,ActivityDatos.class);
+        startActivity(i);
+
+    }
+}
